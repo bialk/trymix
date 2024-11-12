210995fb6ebc85d1eceb40f4aad0956257a01e7c
Alex Blinov <alexander@omi.so>
2024-11-12 14:25:28 +0000

    opencl warning fix

--- BlurTests/GaussBlurEngine.cpp
+++ BlurTests/GaussBlurEngine.cpp
@@ -9,7 +9,7 @@
 #ifdef OpenCL_FOUND
 #define CL_HPP_ENABLE_EXCEPTIONS
 #define CL_HPP_MINIMUM_OPENCL_VERSION 200
-// #define CL_HPP_TARGET_OPENCL_VERSION 200
+#define CL_HPP_TARGET_OPENCL_VERSION 200
 #include <CL/cl.hpp>
 
 #define CaseReturnString(x) case x: return #x;
